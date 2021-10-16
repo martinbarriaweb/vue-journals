fcffc8ece80e0ce2e3423ba9933a4b9e4946ca8b
Martin Barria <martinbarriaweb@gmail.com>
2021-10-16 15:08:56 -0300

    base

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hi world</h1>
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-info">Primary</button>
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    goToDaybook(){
+      this.$router.push({name: 'no-entry'})
+    }
+  }
+}
+</script>
